2fd5d66d6830ea3b70b3754f3e3aef71e16fde00
python273 <python273@yandex.ru>
2013-01-13 23:13:30 +0400

    Hello GitHub

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+""" Setup file for vk_api package """
+
+from distutils.core import setup
+setup(name='vk_api',
+      version='4.0',
+      description='Module to use the API VKontakte (vk.com)',
+      author='Kirill Python',
+      author_email='mikeking568@gmail.com',
+      url='https://github.com/python273/vk_api',
+      packages=['vk_api', 'vk_api.upload', 'jconfig'],
+     )
