d8417ae965ce04d24d11e3ceef4888088a5ffd96
python273 <mikeking568@gmail.com>
2013-09-06 20:55:41 +0400

    4.6
    License Apache2
    Api version 5.0
    Load photos in messages

--- vk_api/__init__.py
+++ vk_api/__init__.py
@@ -1,11 +1,21 @@
+'''
+@author: Kirill Python
+@contact: http://vk.com/python273
+@license Apache License, Version 2.0, see LICENSE file
+
+Copyright (C) 2013
+'''
+
 __author__ = "Kirill Python"
-__version__ = "4.5"
+__version__ = "4.6"
 __email__ = "mikeking568@gmail.com"
 __contact__ = "https://vk.com/python273"
 
-
 import sys
+
 if sys.version_info[0] == 2:
     from vk_api import *
+    from vk_upload import *
 else:
     from .vk_api import *
+    from .vk_upload import *
