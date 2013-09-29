2dc5eb7205a89434c0c6a17a4f8dc12d10b2e9cc
python273 <python273@yandex.ru>
2013-09-29 15:41:24 +0400

    4.6.2
    fix example.py for new api
    encoding in first line

--- jconfig/__init__.py
+++ jconfig/__init__.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+
 '''
 @author: Kirill Python
 @contact: http://vk.com/python273
--- jconfig/jconfig.py
+++ jconfig/jconfig.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+
 '''
 @author: Kirill Python
 @contact: http://vk.com/python273
@@ -6,7 +8,6 @@
 Copyright (C) 2013
 '''
 
-# -*- coding: utf-8 -*-
 import os
 import json
 
