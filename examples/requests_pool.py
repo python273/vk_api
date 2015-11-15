7c7c7f659136004db04565f7100ac67cd8a937fe
python273 <python273@yandex.ru>
2015-11-16 01:13:45 +0300

    Requests pool

---
+++ examples/requests_pool.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+
+"""
+@author: Kirill Python
+@contact: https://vk.com/python273
+"""
+
+import vk_api
+
+
+def main():
+    """ Пример работы с VkRequestsPool """
+
+    login, password = 'python@vk.com', 'mypassword'
+    vk = vk_api.VkApi(login, password)
+
+    try:
+        vk.authorization()
+    except vk_api.AuthorizationError as error_msg:
+        print(error_msg)
+        return
+
+    """
+        TODO: write description
+    """
+
+    friends = {}
+
+    with vk_api.VkRequestsPool(vk) as pool:
+        for user_id in [1, 183433824]:
+            friends[user_id] = pool.method('friends.get', {'user_id': user_id})
+
+    print(friends)
+
+    with vk_api.VkRequestsPool(vk) as pool:
+        friends = pool.method('friends.get')
+        status = pool.method('status.get')
+
+    print(friends)
+    print(status)
+
+if __name__ == '__main__':
+    main()
