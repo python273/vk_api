3214eb2fb30cf137fe79eceddebe8ccdbcf98e59
python273 <python273@yandex.ru>
2016-01-12 08:28:16 +0300

    Update examples

--- examples/requests_pool.py
+++ examples/requests_pool.py
@@ -12,10 +12,10 @@ def main():
     """ Пример работы с VkRequestsPool """
 
     login, password = 'python@vk.com', 'mypassword'
-    vk = vk_api.VkApi(login, password)
+    vk_session = vk_api.VkApi(login, password)
 
     try:
-        vk.authorization()
+        vk_session.authorization()
     except vk_api.AuthorizationError as error_msg:
         print(error_msg)
         return
@@ -26,13 +26,13 @@ def main():
 
     friends = {}
 
-    with vk_api.VkRequestsPool(vk) as pool:
+    with vk_api.VkRequestsPool(vk_session) as pool:
         for user_id in [1, 183433824]:
             friends[user_id] = pool.method('friends.get', {'user_id': user_id})
 
     print(friends)
 
-    with vk_api.VkRequestsPool(vk) as pool:
+    with vk_api.VkRequestsPool(vk_session) as pool:
         friends = pool.method('friends.get')
         status = pool.method('status.get')
 
