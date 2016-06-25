9291b7f7f1c2ad9f2ea7d846e4865d2d9c6ec27a
python273 <python273@yandex.ru>
2016-06-26 02:43:07 +0300

    Update version 7.1

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ from distutils.core import setup
 
 setup(
     name='vk_api',
-    version='7.0',
+    version='7.1',
     author='Kirill Python',
     author_email='python273@ya.ru',
     url='https://github.com/python273/vk_api',
