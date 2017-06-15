57ab2ce6befff61735c01b54892e3a3357daa67b
python273 <iam@python273.pw>
2017-06-15 15:01:19 +0300

    Update jconfig

--- jconfig/memory.py
+++ jconfig/memory.py
@@ -7,38 +7,12 @@
 Copyright (C) 2017
 """
 
+from .base import BaseConfig
 
-class MemoryConfig(object):
 
-    __slots__ = ('_section', '_settings')
-
-    def __init__(self, section, filename=None, settings=None):
-        self._section = section
-
-        if settings is not None:
-            self._settings = settings
-        else:
-            self._settings = {}
-
-        self._settings.setdefault(section, {})
-
-    def __getattr__(self, name):
-        return self._settings[self._section].get(name)
-
-    def __getitem__(self, key):
-        return self.__getattr__(key)
-
-    def __setattr__(self, name, value):
-        if name.startswith('_'):
-            return super(MemoryConfig, self).__setattr__(name, value)
-
-        self._settings[self._section][name] = value
-
-    def __setitem__(self, key, value):
-        return self.__setattr__(key, value)
-
-    def clear_section(self):
-        self._settings[self._section] = {}
+class MemoryConfig(BaseConfig):
+    def load(self, settings=None, **kwargs):
+        return {} if settings is None else settings
 
     def save(self):
         pass
