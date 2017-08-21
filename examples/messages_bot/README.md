3f1040fbf3e9bac26efbf16086d017b5ea61a43a
python273 <iam@python273.pw>
2017-08-21 19:55:10 +0300

    Add bot example

---
+++ examples/messages_bot/README.md
@@ -0,0 +1,4 @@
+Бот работает через [longpoll](https://vk.com/dev/using_longpoll)
+и использует [DuckDuckGo API](http://api.duckduckgo.com/) для получения ответа
+
+![Пример работы бота](bot.png)
---
+++ examples/messages_bot/messages_bot.py
@@ -0,0 +1,78 @@
+# -*- coding: utf-8 -*-
+
+import requests
+
+import vk_api
+from vk_api import VkUpload
+from vk_api.longpoll import VkLongPoll, VkEventType
+
+
+def main():
+    session = requests.Session()
+
+    # Авторизация пользователя:
+    """
+    login, password = 'python@vk.com', 'mypassword'
+    vk_session = vk_api.VkApi(login, password)
+
+    try:
+        vk_session.auth()
+    except vk_api.AuthError as error_msg:
+        print(error_msg)
+        return
+    """
+
+    # Авторизация группы:
+    # при передаче token вызывать vk_session.auth не нужно
+    """
+    vk_session = vk_api.VkApi(token='токен с доступом к сообщениям и фото')
+    """
+
+    vk = vk_session.get_api()
+
+    upload = VkUpload(vk_session)  # Для загрузки изображений
+    longpoll = VkLongPoll(vk_session)
+
+    for event in longpoll.listen():
+        if event.type == VkEventType.MESSAGE_NEW and event.to_me:
+            print('id{}: "{}"'.format(event.user_id, event.text), end=' ')
+
+            response = session.get(
+                'http://api.duckduckgo.com/',
+                params={
+                    'q': event.text,
+                    'format': 'json'
+                }
+            ).json()
+
+            text = response.get('AbstractText')
+            image_url = response.get('Image')
+
+            if not text:
+                vk.messages.send(
+                    user_id=event.user_id,
+                    message='No results'
+                )
+                print('no results')
+                continue
+
+            attachments = []
+
+            if image_url:
+                image = session.get(image_url, stream=True)
+                photo = upload.photo_messages(photos=image.raw)[0]
+
+                attachments.append(
+                    'photo{}_{}'.format(photo['owner_id'], photo['id'])
+                )
+
+            vk.messages.send(
+                user_id=event.user_id,
+                attachment=','.join(attachments),
+                message=text
+            )
+            print('ok')
+
+
+if __name__ == '__main__':
+    main()
