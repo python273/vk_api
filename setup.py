c506e2d67b52779d07211e61d8ccf712e715a2a4
python273 <iam@python273.pw>
2017-09-08 07:15:16 +0300

    Update version to 9.1

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ Copyright (C) 2017
 
 setup(
     name='vk_api',
-    version='9.0',
+    version='9.1',
     author='python273',
     author_email='whoami@python273.pw',
     url='https://github.com/python273/vk_api',
