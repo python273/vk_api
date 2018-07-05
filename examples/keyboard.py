1a1b1ac92a3065fd1d09993880035f197491c303
Igor <igorek20045565@gmail.com>
2018-07-06 00:13:28 +0300

    Example под общий дизайн

--- examples/keyboard.py
+++ examples/keyboard.py
@@ -1,21 +1,25 @@
-from vk_api import VkApi
-from vk_api import VkKeyboard
+import vk_api
 
 
-vk = VkApi(token='your_api_token')
+def main():
+    vk = vk_api.VkApi(token='your_api_token')
 
-keyboard = VkKeyboard(one_time=True)
+    keyboard = vk_api.VkKeyboard(one_time=True)
 
-keyboard.add_line()  # Создание первой строки
-keyboard.add_button('Белая кнопка', color='default')
-keyboard.add_button('Зелёная кнопка', color='positive')
+    keyboard.add_line()  # Создание первой строки
+    keyboard.add_button('Белая кнопка', color='default')
+    keyboard.add_button('Зелёная кнопка', color='positive')
 
-keyboard.add_line()  # Переход на вторую строку
-keyboard.add_button('Красная кнопка', color='negative')
+    keyboard.add_line()  # Переход на вторую строку
+    keyboard.add_button('Красная кнопка', color='negative')
 
-keyboard.add_line()  # Третья строка
-keyboard.add_button('Синяя кнопка', color='primary')
+    keyboard.add_line()  # Третья строка
+    keyboard.add_button('Синяя кнопка', color='primary')
 
-vk.method('messages.send', {'peer_id': 123456,  # peer_id получателя
-                            'message': 'Пример клавиатуры',
-                            'keyboard': keyboard.get_keyboard()})
+    vk.method('messages.send', {'peer_id': 123456,  # peer_id получателя
+                                'message': 'Пример клавиатуры',
+                                'keyboard': keyboard.get_keyboard()})
+
+
+if __name__ == '__main__':
+    main()
