e157c13d082aa2b8915b72823b8e58ccc231db3a
python273 <iam@python273.pw>
2018-07-10 02:27:43 +0300

    Refactor VkKeyboard

--- examples/keyboard.py
+++ examples/keyboard.py
@@ -1,25 +1,30 @@
+# -*- coding: utf-8 -*-
 import vk_api
+from vk_api.keyboard import KeyboardColor
 
 
 def main():
-    vk = vk_api.VkApi(token='your_api_token')
+    """ Пример создания клавиатуры для отправки ботом """
+
+    vk_session = vk_api.VkApi(token='bot_api_token')
+    vk = vk_session.get_api()
 
     keyboard = vk_api.VkKeyboard(one_time=True)
 
-    Colors = vk_api.keyboard.KeyboardColors
-
-    keyboard.add_button('Белая кнопка', color=Colors.DEFAULT.value)
-    keyboard.add_button('Зелёная кнопка', color=Colors.POSITIVE.value)
+    keyboard.add_button('Белая кнопка', color=KeyboardColor.DEFAULT)
+    keyboard.add_button('Зелёная кнопка', color=KeyboardColor.POSITIVE)
 
     keyboard.add_line()  # Переход на вторую строку
-    keyboard.add_button('Красная кнопка', color=Colors.NEGATIVE.value)
+    keyboard.add_button('Красная кнопка', color=KeyboardColor.NEGATIVE)
 
-    keyboard.add_line()  # Третья строка
-    keyboard.add_button('Синяя кнопка', color=Colors.PRIMARY.value)
+    keyboard.add_line()
+    keyboard.add_button('Синяя кнопка', color=KeyboardColor.PRIMARY)
 
-    vk.method('messages.send', {'peer_id': 123456,  # peer_id получателя
-                                'message': 'Пример клавиатуры',
-                                'keyboard': keyboard.get_keyboard()})
+    vk.messages.send(
+        peer_id=123456,
+        message='Пример клавиатуры',
+        keyboard=keyboard.get_keyboard()
+    )
 
 
 if __name__ == '__main__':
--- vk_api/keyboard.py
+++ vk_api/keyboard.py
@@ -1,65 +1,90 @@
 from enum import Enum
+
+import six
+
 from .utils import sjson_dumps
 
 
-class KeyboardColors(Enum):
-    """ Допустимые цвета кнопок.
-    """
-    PRIMARY = 'primary'    # синяя
-    DEFAULT = 'default'    # белая
-    NEGATIVE = 'negative'  # красная
-    POSITIVE = 'positive'  # зелёная
+class KeyboardColor(Enum):
+    """ Возможные цвета кнопок """
+    PRIMARY = 'primary' # синяя
+    DEFAULT = 'default' # белая
+    NEGATIVE = 'negative' # красная
+    POSITIVE = 'positive' # зелёная
 
 
 class VkKeyboard(object):
-    """
-    :param one_time: Если True, клавиатура исчезнет сразу после нажатия на кнопку.
+    """ Класс для создания клавиатуры для бота (https://vk.com/dev/bots_docs_3)
+
+    :param one_time: Если True, клавиатура исчезнет после нажатия на кнопку
     :type one_time: bool
     """
-    def __init__(self, one_time=False):
-        self.keyboard = {}
-        self.keyboard['one_time'] = one_time
-        self.keyboard['buttons'] = []
-        self.keyboard['buttons'].append([])
 
-        self.line = 0
+    __slots__ = ('one_time', 'lines', 'keyboard')
+
+    def __init__(self, one_time=False):
+        self.one_time = one_time
+        self.lines = [[]]
+
+        self.keyboard = {
+            'one_time': self.one_time,
+            'lines': self.lines
+        }
 
     def get_keyboard(self):
-        """ Возвращает json клавиатуры
-        """
+        """ Получить json клавиатуры """
         return sjson_dumps(self.keyboard)
 
     @classmethod
     def get_empty_keyboard(cls):
-        """ Возвращает json пустой клавиатуры.
+        """ Получить json пустой клавиатуры.
             Если отправить пустую клавиатуру, текущая у пользователя исчезнет.
         """
         keyboard = cls()
-        keyboard.keyboard['buttons'] = []
+        keyboard.keyboard['lines'] = []
         return keyboard.get_keyboard()
 
-    def add_button(self, label, color='default', payload=None):
-        """
+    def add_button(self, label, color=KeyboardColor.DEFAULT, payload=None):
+        """ Добавить кнопку. Максимальное количество кнопок на строке - 4
+
         :param label: Надпись на кнопке и текст, отправляющийся при её нажатии.
+        :type: str
 
         :param color: цвет кнопки.
-            См. :class:`KeyboardColors`
+        :type color: KeyboardColor or str
 
         :param payload: Параметр для callback api
-
-        Максимальное количество кнопок на строке - 4
+        :type payload: str or list or dict
         """
-        button = {}
-        button['action'] = {}
-        button['action']['type'] = 'text'
-        button['action']['payload'] = payload
-        button['action']['label'] = label
-        button['color'] = color
-        self.keyboard['buttons'][self.line].append(button)
+
+        current_line = self.lines[-1]
+
+        if len(current_line) >= 4:
+            raise ValueError('Max 4 buttons on a line')
+
+        color_value = color
+
+        if isinstance(color, KeyboardColor):
+            color_value = color_value.value
+
+        if not isinstance(payload, six.string_types):
+            payload = sjson_dumps(payload)
+
+        current_line.append({
+            'color': color_value,
+            'action': {
+                'type': 'text',
+                'payload': payload,
+                'label': label,
+            }
+        })
 
     def add_line(self):
         """ Создаёт новую строчку, на которой можно размещать кнопки.
             Максимальное количество строк - 10
         """
-        self.line += 1
-        self.keyboard['buttons'].append([])
+
+        if len(self.lines) >= 10:
+            raise ValueError('Max 10 lines')
+
+        self.lines.append([])
