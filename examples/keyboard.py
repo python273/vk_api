0aeea70b91abccb258af07a973ed5f7d82eb7a6a
Igor <igorek20045565@gmail.com>
2018-07-09 18:57:29 +0300

    Enum in example

--- examples/keyboard.py
+++ examples/keyboard.py
@@ -6,14 +6,16 @@ def main():
 
     keyboard = vk_api.VkKeyboard(one_time=True)
 
-    keyboard.add_button('Белая кнопка', color='default')
-    keyboard.add_button('Зелёная кнопка', color='positive')
+    Colors = vk_api.keyboard.KeyboardColors
+
+    keyboard.add_button('Белая кнопка', color=Colors.DEFAULT.value)
+    keyboard.add_button('Зелёная кнопка', color=Colors.POSITIVE.value)
 
     keyboard.add_line()  # Переход на вторую строку
-    keyboard.add_button('Красная кнопка', color='negative')
+    keyboard.add_button('Красная кнопка', color=Colors.NEGATIVE.value)
 
     keyboard.add_line()  # Третья строка
-    keyboard.add_button('Синяя кнопка', color='primary')
+    keyboard.add_button('Синяя кнопка', color=Colors.PRIMARY.value)
 
     vk.method('messages.send', {'peer_id': 123456,  # peer_id получателя
                                 'message': 'Пример клавиатуры',
