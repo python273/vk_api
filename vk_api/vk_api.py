e0c54b176dc5bd277124ca9ea46601e9002a69ed
Igor <helow@helow19274.tk>
2018-09-24 15:13:39 +0300

    Version 5.85, lp version 3, setup.py open encoding (#181)
    * Version 5.85, lp version 3

--- vk_api/vk_api.py
+++ vk_api/vk_api.py
@@ -85,7 +85,7 @@ class VkApi(object):
     def __init__(self, login=None, password=None, token=None,
                  auth_handler=None, captcha_handler=None,
                  config=jconfig.Config, config_filename='vk_config.v2.json',
-                 api_version='5.84', app_id=6222115, scope=DEFAULT_USER_SCOPE,
+                 api_version='5.85', app_id=6222115, scope=DEFAULT_USER_SCOPE,
                  client_secret=None):
 
         self.login = login
