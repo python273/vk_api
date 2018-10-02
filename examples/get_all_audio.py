4865c613a7e1f85d90982b62b162b5186860b218
python273 <iam@python273.pw>
2018-10-02 21:49:14 +0300

    Update get_all_audio example

--- examples/get_all_audio.py
+++ examples/get_all_audio.py
@@ -19,22 +19,21 @@ def main():
 
     vkaudio = VkAudio(vk_session)
 
-    artists = collections.Counter()
-
-    for track in vkaudio.get_iter(-99463083):
-        artists[track['artist']] += 1
+    artists = collections.Counter(
+        track['artist'] for track in vkaudio.get_iter()
+    )
 
     # Составляем рейтинг первых 15
-    print('\nTop 15:')
+    print('Top 15:')
     for artist, tracks in artists.most_common(15):
         print('{} - {} tracks'.format(artist, tracks))
 
     # Ищем треки самого популярного
     most_common_artist = artists.most_common(1)[0][0]
 
-    print('\nSearch for', most_common_artist)
+    print('\nSearching for {}:'.format(most_common_artist))
 
-    tracks = vkaudio.search(q=most_common_artist)[:10]
+    tracks = vkaudio.search(q=most_common_artist, count=10)
 
     for n, track in enumerate(tracks, 1):
         print('{}. {} {}'.format(n, track['title'], track['url']))
