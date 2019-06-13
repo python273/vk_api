b015ee528802054ee899ab3a36a953f1862b0179
markeloff <klassromanov@gmail.com>
2019-06-13 17:23:56 +0300

    add new button types

--- vk_api/keyboard.py
+++ vk_api/keyboard.py
@@ -1,8 +1,7 @@
 # -*- coding: utf-8 -*-
 """
-:authors: python273, Helow19274
+:authors: python273, Helow19274, klassik2k16
 :license: Apache License, Version 2.0, see LICENSE file
-
 :copyright: (c) 2019 python273
 """
 
@@ -28,10 +27,25 @@ class VkKeyboardColor(Enum):
     #: Зелёная
     POSITIVE = 'positive'
 
+class VkKeyboardButton(Enum):
+    """ Возможные типы кнопки """
+
+    #: Кнопка с текстом
+    TEXT = "text"
+
+    #: Кнопка с местоположением
+    LOCATION = "location"
+
+    #: Кнопка с оплатой через VKPay
+    VKPAY = "vkpay"
+
+    #: Кнопка с приложением VK Apps
+    VKAPPS = "open_app"
+
+
 
 class VkKeyboard(object):
     """ Класс для создания клавиатуры для бота (https://vk.com/dev/bots_docs_3)
-
     :param one_time: Если True, клавиатура исчезнет после нажатия на кнопку
     :type one_time: bool
     """
@@ -60,15 +74,13 @@ class VkKeyboard(object):
         keyboard.keyboard['buttons'] = []
         return keyboard.get_keyboard()
 
-    def add_button(self, label, color=VkKeyboardColor.DEFAULT, payload=None):
-        """ Добавить кнопку. Максимальное количество кнопок на строке - 4
+    def add_text_button(self, label, color=VkKeyboardColor.DEFAULT, payload=None):
+        """ Добавить кнопку с текстом. Максимальное количество кнопок на строке - 4
 
         :param label: Надпись на кнопке и текст, отправляющийся при её нажатии.
         :type label: str
-
         :param color: цвет кнопки.
         :type color: VkKeyboardColor or str
-
         :param payload: Параметр для callback api
         :type payload: str or list or dict
         """
@@ -95,12 +107,93 @@ class VkKeyboard(object):
             }
         })
 
+    def add_location_button(self, payload=None):
+        """ Добавить кнопку с местоположением. Всегда занимает всю ширину линии.
+
+        :param payload: Параметр для callback api
+        :type payload: str or list or dict
+        """
+
+        current_line = self.lines[-1]
+
+        if len(current_line) != 0:
+            raise ValueError('A button of type Location occupies the entire line of the keyboard, besides it on the line of buttons cannot be.')
+
+        if payload is not None and not isinstance(payload, six.string_types):
+            payload = sjson_dumps(payload)
+
+        current_line.append({
+            'action': {
+                'type': VkKeyboardButton.LOCATION,
+                'payload': payload
+            }
+        })
+
+    def add_vkpay_button(self, hash, payload=None):
+        """ Добавить кнопку с оплатой с помощью VKPay. Всегда занимает всю ширину линии.
+
+        :param hash: Параметры платежа VKPay и ID приложения (в поле aid) разделённые &
+        :type hash: str
+        :param payload: Параметр для совместимости со старыми клиентами
+        :type payload: str or list or dict
+        """
+
+        current_line = self.lines[-1]
+
+        if len(current_line) != 0:
+            raise ValueError('A button of type VKPay occupies the entire line of the keyboard, besides it on the line of buttons cannot be.')
+
+        if payload is not None and not isinstance(payload, six.string_types):
+            payload = sjson_dumps(payload)
+
+        current_line.append({
+            'action': {
+                'type': VkKeyboardButton.VKPAY,
+                'payload': payload,
+                'hash': hash
+            }
+        })
+
+    def add_vkapps_button(self, app_id, owner_id, label, hash, payload=None):
+        """ Добавить кнопку с приложением VK Apps. Всегда занимает всю ширину линии.
+
+        :param app_id: Идентификатор вызываемого приложения с типом VK Apps
+        :type app_id: int
+        :param owner_id: Идентификатор сообщества, в котором установлено приложение, если требуется открыть в контексте сообщества
+        :type owner_id: int
+        :param label: Название приложения, указанное на кнопке
+        :type label: str
+        :param hash: хэш для навигации в приложении, будет передан в строке параметров запуска после символа #
+        :type hash: str
+        :param payload: Параметр для совместимости со старыми клиентами
+        :type payload: str or list or dict
+        """
+
+        current_line = self.lines[-1]
+
+        if len(current_line) != 0:
+            raise ValueError('A button of type VKApps occupies the entire line of the keyboard, besides it on the line of buttons cannot be.')
+
+        if payload is not None and not isinstance(payload, six.string_types):
+            payload = sjson_dumps(payload)
+
+        current_line.append({
+            'action': {
+                'type': VkKeyboardButton.VKAPPS,
+                'app_id': app_id,
+                'owner_id': owner_id,
+                'label': label,
+                'payload': payload,
+                'hash': hash
+            }
+        })
+
     def add_line(self):
-        """ Создаёт новую строчку, на которой можно размещать кнопки.
+        """ Создаёт новую строку, на которой можно размещать кнопки.
         Максимальное количество строк - 10.
         """
 
         if len(self.lines) >= 10:
             raise ValueError('Max 10 lines')
 
-        self.lines.append([])
+self.lines.append([])
