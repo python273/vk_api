09b555864e862015cf3bb0f44481dafae9007901
TechnoStrife <plotnikov7rodion@gmail.com>
2019-08-31 09:18:06 +0300

    Add VkUpload.photo_market

--- vk_api/upload.py
+++ vk_api/upload.py
@@ -202,6 +202,49 @@ class VkUpload(object):
 
         return self.vk.photos.saveWallPhoto(**values)
 
+    def photo_market(self, photos, group_id, main_photo=False,
+                     crop_x=None, crop_y=None, crop_width=None):
+        """ Загрузка изображений для товаров в магазине
+
+        :param photos: путь к изображению(ям) или file-like объект(ы)
+        :type photos: str or list
+
+        :param group_id: идентификатор сообщества, для которого необходимо загрузить фотографию товара
+        :type group_id: int
+        :param main_photo: является ли фотография обложкой товара
+        :type main_photo: bool
+        :param crop_x: координата x для обрезки фотографии (верхний правый угол)
+        :type crop_x: int
+        :param crop_y: координата y для обрезки фотографии (верхний правый угол)
+        :type crop_y: int
+        :param crop_width: ширина фотографии после обрезки в px
+        :type crop_width: int
+        """
+
+        values = {
+            'main_photo': main_photo
+        }
+
+        if group_id is not None:
+            values['group_id'] = group_id
+
+        if crop_x is not None:
+            values['crop_x'] = crop_x
+        if crop_y is not None:
+            values['crop_y'] = crop_y
+        if crop_width is not None:
+            values['crop_width'] = crop_width
+
+        response = self.vk.photos.getMarketUploadServer(**values)
+        url = response['upload_url']
+
+        with FilesOpener(photos) as photos_files:
+            response = self.http.post(url, files=photos_files)
+
+        values.update(response.json())
+
+        return self.vk.photos.saveMarketPhoto(**values)
+
     def audio(self, audio, artist, title):
         """ Загрузка аудио
 
