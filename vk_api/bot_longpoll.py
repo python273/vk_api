a4b5f83ef9188575fc8e8c5582c30250f51ba4ba
Марат Недогимов <56220501+nedogimov@users.noreply.github.com>
2019-11-15 21:55:36 +0500

    Update bot_longpoll.py

--- vk_api/bot_longpoll.py
+++ vk_api/bot_longpoll.py
@@ -166,7 +166,7 @@ class VkBotMessageEvent(VkBotEvent):
         self.from_group = False
         self.chat_id = None
 
-        peer_id = self.message.peer_id
+        peer_id = self.obj.peer_id or self.message.peer_id
         
         if peer_id < 0:
             self.from_group = True
