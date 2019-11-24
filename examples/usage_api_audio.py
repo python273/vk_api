5b3a621be5ea4be6ad26f3cf607ce4c31303fdce
Марат Недогимов <56220501+nedogimov@users.noreply.github.com>
2019-11-24 18:47:28 +0500

    Пример использования полноценного апи аудио

---
+++ examples/usage_api_audio.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+import requests
+
+import vk_api
+from vk_api.audio import VkAudio
+
+
+""" Получение токена: """
+""" 1. Установка php """
+""" 2. Клонирование репозитория https://github.com/vodka2/vk-audio-token """
+""" 3. Переход в vk-audio-token/src/cli """
+""" 4. Получение токена: php vk-audio-token.php -m логин пароль"""
+""" 5. Готово. """
+
+
+def main():
+    """ Использование апи аудио """
+
+    token = 'token'
+    
+    session = requests.Session()
+    session.headers.update({
+                'User-agent': "KateMobileAndroid/52.1 lite-445 (Android 4.4.2; SDK 19; x86; unknown Android SDK built for x86; en)"})
+
+    vk_session = vk_api.VkApi(token=token, session=session)  #Варнинг о логине игнорируем иначе не будет работать.
+
+    api = vk_session.get_api()
+    
+    search = api.audio.search(q="123") #пример поиска
+    print(search)
+
+
+if __name__ == '__main__':
+    main()
