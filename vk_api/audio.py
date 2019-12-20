57e6ee1609a2df81b421fcb72ddec18a102b327f
qwertyadrian <qwertyadrian@yandex.ru>
2019-12-20 16:29:27 +0300

    Updated method description in VkAudio.get_post_audio

--- vk_api/audio.py
+++ vk_api/audio.py
@@ -222,6 +222,11 @@ class VkAudio(object):
         return decode_audio_url(link, self.user_id)
 
     def get_post_audio(self, owner_id, post_id):
+        """ Получить список аудиозаписей поста пользователя или сообщества
+
+        :param owner_id: ID владельца (отрицательные значения для групп)
+        :param owner_id: ID поста
+        """
         response = self._vk.http.get(
             'https://m.vk.com/wall{}_{}'.format(owner_id, post_id)
         )
