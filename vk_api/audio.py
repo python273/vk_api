7005df722658a424ed00891dee86a64157d95996
Adrian <20292956+qwertyadrian@users.noreply.github.com>
2020-01-05 02:57:02 +0300

    Получение аудиозаписей из поста пользователя или сообщества (#323)

--- vk_api/audio.py
+++ vk_api/audio.py
@@ -36,6 +36,10 @@ class VkAudio(object):
         self.user_id = vk.method('users.get')[0]['id']
         self._vk = vk
 
+        self._vk.http.cookies.update({
+            'remixmdevice': '1920/1080/1/!!-!!!!'
+        })
+
     def get_iter(self, owner_id=None, album_id=None, access_hash=None):
         """ Получить список аудиозаписей пользователя (по частям)
 
@@ -220,6 +224,24 @@ class VkAudio(object):
         bs = BeautifulSoup(response.text, 'html.parser')
         link = bs.select_one('.ai_body input[type=hidden]').attrs['value']
         return decode_audio_url(link, self.user_id)
+
+    def get_post_audio(self, owner_id, post_id):
+        """ Получить список аудиозаписей из поста пользователя или группы
+
+        :param owner_id: ID владельца (отрицательные значения для групп)
+        :param post_id: ID поста
+        """
+        response = self._vk.http.get(
+            'https://m.vk.com/wall{}_{}'.format(owner_id, post_id)
+        )
+
+        tracks = scrap_data(
+            response.text,
+            self.user_id,
+            filter_root_el={'class': 'audios_list'}
+        )
+
+        return tracks
 
 
 def scrap_data(html, user_id, filter_root_el=None, convert_m3u8_links=True):
