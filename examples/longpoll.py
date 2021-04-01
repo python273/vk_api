92edec8df205ea6104d8377862573208a3ee132c
mlneko <70600003+mlneko@users.noreply.github.com>
2021-04-01 18:59:43 +0300

    Update longpoll.py

--- examples/longpoll.py
+++ examples/longpoll.py
@@ -1,26 +1,10 @@
 # -*- coding: utf-8 -*-
 import vk_api
 from vk_api.longpoll import VkLongPoll, VkEventType
+from requests.exceptions import RequestException
 
 
-"""
-Пример не работает с дефолтным app_id
-
-Подробнее об ограничении доступа к сообщениям:
-https://vk.com/dev/messages_api
-
-Обсуждение:
-https://github.com/python273/vk_api/issues/219
-"""
-
-
-def main():
-    """ Пример использования longpoll
-
-        https://vk.com/dev/using_longpoll
-        https://vk.com/dev/using_longpoll_2
-    """
-
+def connect():
     login, password = 'python@vk.com', 'mypassword'
     vk_session = vk_api.VkApi(login, password)
 
@@ -32,46 +16,73 @@ def main():
 
     longpoll = VkLongPoll(vk_session)
 
-    for event in longpoll.listen():
 
-        if event.type == VkEventType.MESSAGE_NEW:
-            print('Новое сообщение:')
-
-            if event.from_me:
-                print('От меня для: ', end='')
-            elif event.to_me:
-                print('Для меня от: ', end='')
-
-            if event.from_user:
-                print(event.user_id)
-            elif event.from_chat:
-                print(event.user_id, 'в беседе', event.chat_id)
-            elif event.from_group:
-                print('группы', event.group_id)
-
-            print('Текст: ', event.text)
-            print()
-
-        elif event.type == VkEventType.USER_TYPING:
-            print('Печатает ', end='')
-
-            if event.from_user:
-                print(event.user_id)
-            elif event.from_group:
-                print('администратор группы', event.group_id)
-
-        elif event.type == VkEventType.USER_TYPING_IN_CHAT:
-            print('Печатает ', event.user_id, 'в беседе', event.chat_id)
-
-        elif event.type == VkEventType.USER_ONLINE:
-            print('Пользователь', event.user_id, 'онлайн', event.platform)
-
-        elif event.type == VkEventType.USER_OFFLINE:
-            print('Пользователь', event.user_id, 'оффлайн', event.offline_type)
-
-        else:
-            print(event.type, event.raw[1:])
+"""
+Пример не работает с дефолтным app_id
+Подробнее об ограничении доступа к сообщениям:
+https://vk.com/dev/messages_api
+Обсуждение:
+https://github.com/python273/vk_api/issues/219
+"""
 
 
+def main():
+    """ Пример использования longpoll
+        https://vk.com/dev/using_longpoll
+        https://vk.com/dev/using_longpoll_2
+    """
+
+
+
+    while True:
+        try:
+            
+            for event in longpoll.listen():
+
+                if event.type == VkEventType.MESSAGE_NEW:
+                    print('Новое сообщение:')
+
+                    if event.from_me:
+                        print('От меня для: ', end='')
+                    elif event.to_me:
+                        print('Для меня от: ', end='')
+
+                    if event.from_user:
+                        print(event.user_id)
+                    elif event.from_chat:
+                        print(event.user_id, 'в беседе', event.chat_id)
+                    elif event.from_group:
+                        print('группы', event.group_id)
+
+                    print('Текст: ', event.text)
+                    print()
+
+                elif event.type == VkEventType.USER_TYPING:
+                    print('Печатает ', end='')
+
+                    if event.from_user:
+                        print(event.user_id)
+                    elif event.from_group:
+                        print('администратор группы', event.group_id)
+
+                elif event.type == VkEventType.USER_TYPING_IN_CHAT:
+                    print('Печатает ', event.user_id, 'в беседе', event.chat_id)
+
+                elif event.type == VkEventType.USER_ONLINE:
+                    print('Пользователь', event.user_id, 'онлайн', event.platform)
+
+                elif event.type == VkEventType.USER_OFFLINE:
+                    print('Пользователь', event.user_id, 'оффлайн', event.offline_type)
+
+                else:
+                    print(event.type, event.raw[1:])
+
+        except RequestException:
+                """Переподключение в случае долгой неактивности или перезагрузки серверов"""
+                
+                connect()
+                    
+                    
+                    
 if __name__ == '__main__':
     main()
