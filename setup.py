d5de703cb457016019303b58765480be94bb7177
python273 <iam@python273.pw>
2022-05-01 20:18:08 +0300

    Update version to 11.9.8

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ from setuptools import setup
 """
 
 
-version = '11.9.7'
+version = '11.9.8'
 
 with open('README.md', encoding='utf-8') as f:
     long_description = f.read()
--- vk_api/__init__.py
+++ vk_api/__init__.py
@@ -14,5 +14,5 @@ from .vk_api import VkApi
 
 
 __author__ = 'python273'
-__version__ = '11.9.7'
+__version__ = '11.9.8'
 __email__ = 'vk_api@python273.pw'
