efd2909e6506523f90878ce853e0bf9854689c68
Kirill Vercetti <office@kyzima-spb.com>
2025-02-05 21:30:25 +0300

    Рефакторинг функции generate_device_id

--- vk_api/utils.py
+++ vk_api/utils.py
@@ -9,6 +9,8 @@
 from __future__ import print_function
 
 import random
+import secrets
+import string
 
 try:
     import simplejson as json
@@ -169,10 +171,6 @@ def enable_debug_mode(vk_session, print_content=False):
 
 
 def generate_device_id(n: int = 21) -> str:
-    """
-    Generates a random string of length n from a given set of characters.
-
-    The implementation is taken from the VK source code.
-    """
-    charset = 'useandom-26T198340PX75pxJACKVERYMINDBUSHWOLF_GQZbfghjklqvwyzrict'
-    return ''.join(random.choice(charset) for _ in range(n))
+    """Generates a random string of length n from a given set of characters."""
+    charset = f'{string.digits}{string.ascii_letters}-_'
+    return ''.join(secrets.choice(charset) for _ in range(n))
